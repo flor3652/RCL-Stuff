d8d54e56f15eaf2de46fd553cc20420d584cc78e
Michael Floren <flor3652@bears.unco.edu>
2015-11-08 15:11:05 -0700

    Initial upload

---
+++ installSwirl/installSwirl.R
@@ -0,0 +1,5 @@
+# If using a computer that has trouble downloading from CRAN, try removing the "#" from in front of the following code and running again
+# chooseCRANmirror(ind=which(getCRANmirrors()[,1] == "USA (CO)"),useHTTPS=FALSE)
+if(!require(swirl)) install.packages("swirl")
+library(swirl)
+swirl()
